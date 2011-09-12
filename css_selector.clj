5df84242d54e0b9ea6a1972ab1041bcca7542fa1
Anders Bengtsson <anders.bengtsson@valtech.se>
2011-09-12 15:14:42 +0200

    refactoring duplicate logic

--- css_selector.clj
+++ css_selector.clj
@@ -2,20 +2,25 @@
   (:require [clojure.xml :as xml]))
 
 
+(defn tag? [tag-name xml-tag]
+  (let [tag-name-keyword (keyword tag-name)]
+    (= tag-name-keyword (:tag xml-tag))))
+
+(defn has-class? [class-name xml-tag]
+  (= class-name (:class (:attrs xml-tag))))
+
 (defn matcher [selector-string]
   (let [with-class-re-matcher (re-matcher #"^(\w+)\.(\w+)$" selector-string)]
     (if (.matches with-class-re-matcher)
                                         ; "tagname.classname" form
       (let [[dummy tag-name class-name] (re-groups with-class-re-matcher)]
-        (let [tag-name-keyword (keyword tag-name)]
           (fn [xml-tag]
             (and
-             (= tag-name-keyword (:tag xml-tag))
-             (= class-name (:class (:attrs xml-tag)))))))
+             (tag? tag-name xml-tag)
+             (has-class? class-name xml-tag))))
                                         ; "tagname" form
-      (let [selector (keyword selector-string)]
-        (fn [xml-tag]
-          (= selector (:tag xml-tag)))))))
+      (fn [xml-tag]
+        (tag? selector-string xml-tag)))))
 
 (defn query [selector xml-input]
   (filter (matcher selector)
